f240402e4e5916fdd2601eb8d1e6b21b019684d9
KauanMP <kauanmoreira1232@gmail.com>
2023-08-16 20:06:56 -0300

    Configurando o dotenv, criando a rota de teste da API e configurando postman

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
 
-const port = 5000;
+const port = process.env.PORT;
 
 const app = express();
 
@@ -10,6 +12,10 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// routes
+const router = require("./routes/Router.js");
+app.use(router);
 
 app.listen(port, () => {
   console.log(`App rodando na porta ${port}`);
