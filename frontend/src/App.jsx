bfa4c182a1dbb99d1fc6277e05bc49b0aa6f9525
KauanMP <kauanmoreira1232@gmail.com>
2023-09-27 09:36:06 -0300

    Verificando se o usuário está autenticado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,16 +11,29 @@ import { useAuth } from "./hooks/useAuth";
 
 function App() {
   const { auth, loading } = useAuth();
-  
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className="container">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+            <Route
+              path="/"
+              element={auth ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={!auth ? <Login /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/register"
+              element={!auth ? <Register /> : <Navigate to="/" />}
+            />
           </Routes>
         </div>
         <Footer />
