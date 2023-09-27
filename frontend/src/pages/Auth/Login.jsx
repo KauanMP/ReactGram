14e961d75ac1783fa1b15323bc00012277dc60c7
KauanMP <kauanmoreira1232@gmail.com>
2023-09-27 18:55:30 -0300

    Finalizando login

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -9,8 +9,13 @@ import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 // Redux
+import { login, reset } from "../../slices/authSlice";
 
 const Login = () => {
+  const dispatch = useDispatch();
+
+  const { loading, error } = useSelector((state) => state.auth);
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -21,7 +26,14 @@ const Login = () => {
       email,
       password,
     };
+
+    dispatch(login(user));
   };
+
+  useEffect(() => {
+    dispatch(reset());
+  }, [dispatch]);
+
   return (
     <div id="login">
       <h2>ReactGram</h2>
@@ -39,7 +51,9 @@ const Login = () => {
           onChange={(e) => setPassword(e.target.value)}
           value={password || ""}
         />
-        <input type="submit" value="Entrar" />
+        {!loading && <input type="submit" value="Entrar" />}
+        {loading && <input type="submit" value="Aguarde..." disabled />}
+        {error && <Message msg={error} type="error" />}
       </form>
       <p>
         Não tem uma conta ? <Link to="/register">Clique aqui</Link>
