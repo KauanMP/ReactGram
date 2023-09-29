a2771b5cfe94c7ac2454a6abcef9557dd3890e3f
KauanMP <kauanmoreira1232@gmail.com>
2023-09-29 13:59:56 -0300

    service para inserir fotos

---
+++ frontend/src/services/photoServices.js
@@ -0,0 +1,22 @@
+import { api, requestConfig } from "../utils/config";
+
+// Publisher an user photo
+const publisherPhoto = async (data, token) => {
+  const config = requestConfig("POST", data, token, true);
+
+  try {
+    const res = await requestConfig(`${api}/photos/`, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const photoService = {
+  publisherPhoto,
+};
+
+export default photoService;
