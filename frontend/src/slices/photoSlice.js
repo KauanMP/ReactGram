162ed7b253d0f9ca1befbe264cc4dbdeb70bfa33
KauanMP <kauanmoreira1232@gmail.com>
2023-09-30 17:10:33 -0300

    Slice de edição de fotos

--- frontend/src/slices/photoSlice.js
+++ frontend/src/slices/photoSlice.js
@@ -37,17 +37,39 @@ export const getUserPhotos = createAsyncThunk(
   }
 );
 
-export const deletePhoto = createAsyncThunk("photo/delete", async (id, thunkAPI) => {
-  const token = thunkAPI.getState().auth.user.token;
+export const deletePhoto = createAsyncThunk(
+  "photo/delete",
+  async (id, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
 
-  const data = await photoServices.deletePhoto(id, token);
+    const data = await photoServices.deletePhoto(id, token);
 
-  if (data.errors) {
-    return thunkAPI.rejectWithValue(data.errors[0]);
+    if (data.errors) {
+      return thunkAPI.rejectWithValue(data.errors[0]);
+    }
+
+    return data;
   }
+);
 
-  return data;
-});
+export const updatePhoto = createAsyncThunk(
+  "photo/update",
+  async (photoData, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
+
+    const data = await photoServices.updatePhoto(
+      { title: photoData.title },
+      photoData.id,
+      token
+    );
+
+    if (data.errors) {
+      return thunkAPI.rejectWithValue(data.errors[0]);
+    }
+
+    return data;
+  }
+);
 
 export const photoSlice = createSlice({
   name: "photo",
@@ -100,6 +122,26 @@ export const photoSlice = createSlice({
         state.message = action.payload.message;
       })
       .addCase(deletePhoto.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+        state.photo = {};
+      })
+      .addCase(updatePhoto.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(updatePhoto.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.photos.map((photo) => {
+          if (photo._id === action.payload.photo._id) {
+            return (photo.title = action.payload.photo.title);
+          }
+        });
+        state.message = action.payload.message;
+      })
+      .addCase(updatePhoto.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload;
         state.photo = {};
