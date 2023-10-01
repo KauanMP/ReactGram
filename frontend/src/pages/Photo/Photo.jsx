cc20cbc46a50810449375ecb993b52708ce42610
KauanMP <kauanmoreira1232@gmail.com>
2023-09-30 22:58:47 -0300

    Criando pagina de post individual

---
+++ frontend/src/pages/Photo/Photo.jsx
@@ -0,0 +1,24 @@
+import "./Photo.css"
+
+import { uploads } from "../../utils/config"
+
+// Components
+import Message from "../../components/Message"
+import { Link } from "react-router-dom"
+
+// Hooks
+import { useEffect, useState } from "react"
+import { UseSelector, useDispatch } from "react-redux/es/hooks/useSelector" 
+import { useParams } from "react-router-dom"
+
+
+// Redux
+
+
+const Photo = () => {
+  return (
+    <div>Photo</div>
+  )
+}
+
+export default Photo
